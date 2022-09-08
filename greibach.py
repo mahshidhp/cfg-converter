8bcd228103b0c481711f3b247ddd848ae88f4308
mahshidhp <mahshidhpour@gmail.com>
2022-09-08 15:19:47 +0430

    move server files to root directory

--- greibach.py
+++ greibach.py
@@ -3,12 +3,18 @@ from converter import Converter
 
 
 class Greibach(Converter):
+
     def convert(self):
         chomsky_converter = Chomsky(self.grammar)
         self.grammar = chomsky_converter.convert()
         self.messages = chomsky_converter.messages
 
+        mapping = dict.fromkeys(self.grammar.non_terminals)
 
+        for i in range(len(self.grammar.rules)):
+            rule = self.grammar.rules[i]
 
         return self.grammar
 
+    def remove_left_recursion(self):
+        pass
